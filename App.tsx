5a5c61a64b5de8c2935d6e20e2297994679cf92b
KDMint <khadijalima2005@gmail.com>
2026-03-03 08:56:47 -0300

    feat: initialize project with Expo and TypeScript configuration

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text><h1>Teste</h1><h2>Isso é um teste</h2></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
